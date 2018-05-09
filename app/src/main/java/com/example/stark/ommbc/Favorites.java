faa6f2a55b29b98be2d4946a3d8ea8ec00f1f552
Alejandro Del Villar <alejandro.delvillar@cetys.edu.mx>
2018-05-08 17:22:59 -0700

    added favorites ADV

---
+++ app/src/main/java/com/example/stark/ommbc/Favorites.java
@@ -0,0 +1,19 @@
+package com.example.stark.ommbc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+//QUOTE list favorites
+public class Favorites extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_problemas, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Favorites");
+        return rootView;
+    }
+}
